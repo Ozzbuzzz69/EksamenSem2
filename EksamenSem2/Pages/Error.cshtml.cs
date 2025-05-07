4fca6870c1fce51c0648b2c74c789988e9df0d27
Oscar Friis Ussing <osu001@edu.zealand.dk>
2025-05-07 10:31:15 +0200

    d

--- EksamenSem2/Pages/Error.cshtml.cs
+++ EksamenSem2/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace EksamenSem2.Pages
+namespace EksamenProjekt2Sem.Pages
 {
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
